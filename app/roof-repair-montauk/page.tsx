590573c03b98ecc2c721875d2051183ef9cbf751
cimhasa3 <cimhasa333@gmail.com>
2024-09-12 15:17:53 -0400

    Update page.tsx

--- app/roof-repair-montauk/page.tsx
+++ app/roof-repair-montauk/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import ProfessionalRoofing from "./components/ProfessionalRoofing";
-
+import ExploringRoofing from "./components/ExploringRoofing";
 import WhyChoose from "./components/WhyChoose";
 import Footer from "./components/Footer";
-
+import Review from "./components/Review";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <ProfessionalRoofing />
- 
+      <Review />
+      <ExploringRoofing />
       <WhyChoose />
       <Footer />
     </div>
