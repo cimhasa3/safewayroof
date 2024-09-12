bcc7b83e6d072e7018032ed8901ac98588e994db
cimhasa3 <cimhasa333@gmail.com>
2024-09-12 10:04:34 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import ProfessionalRoofing from "./components/ProfessionalRoofing";
 import ExploringRoofing from "./components/ExploringRoofing";
 import WhyChoose from "./components/WhyChoose";
 import Footer from "./components/Footer";
-
+import Review from "./components/Review";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <ProfessionalRoofing />
- 
+      <Review />
       <ExploringRoofing />
       <WhyChoose />
       <Footer />
